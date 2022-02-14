6a656919ade15cb7ec0adf4fbc773ee682bccf12
SURAJ-PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2022-02-14 20:16:46 +0530

    Add files via upload

---
+++ DeleteElement.java
@@ -0,0 +1,25 @@
+public class DeleteElement
+{
+
+
+    public static void main(String[] args){
+        int a[]={10,20,30,40,50,60};
+        int delete=30;
+
+        for(int i=0;i<a.length-1;i++){
+            if(a[i]==delete){
+                for(int j=i;j<a.length-1;j++){
+                    a[j]=a[j+1];
+                }
+                break;
+
+            }
+        }
+        for(int i=0;i<a.length-1;i++){
+            System.out.print(a[i]+" ");
+        }
+
+
+    }
+
+}
